fe2ae21559f76fdd27182dfcb46435c56b5e8b29
Jan Schöfer <hello@janschoefer.com>
2018-04-23 11:56:09 +0200

    -Corrected truncation -adjustments in Zabbix template

--- raspberrypi.sh
+++ raspberrypi.sh
@@ -17,19 +17,19 @@ case "$1" in
                 ;;
         coreclock)
                 # Core Clock Speed in Hz
-                /opt/vc/bin/vcgencmd measure_clock core | tr -d "frequency(1)="
+                /opt/vc/bin/vcgencmd measure_clock core | sed s/"frequency(1)="//g
                 ;;
         cpuclock)
                 # ARM Clock Speed in Hz
-                /opt/vc/bin/vcgencmd measure_clock arm | tr -d "frequency(45)="
+                /opt/vc/bin/vcgencmd measure_clock arm | sed s/"frequency(45)="//g
                 ;;
         cpumem)
                 # CPU Memory in MByte
-                vcgencmd get_mem arm | tr -d "arm=" | tr -d "M"
+                vcgencmd get_mem arm | tr -d "arm=M"
                 ;;
         cpuvoltage)
                 # CPU Voltage in Volt
-                /opt/vc/bin/vcgencmd measure_volts | tr -d "volt=" | tr -d "V"
+                /opt/vc/bin/vcgencmd measure_volts | tr -d "volt=V"
                 ;;
         firmwareversion)
                 # Just the naked String of the firmware Version
@@ -37,39 +37,39 @@ case "$1" in
                 ;;
         gpumem)
                 # Graphics memeory in MByte
-                vcgencmd get_mem gpu | tr -d "gpu=" | tr -d "M"
+                vcgencmd get_mem gpu | tr -d "gpu=M"
                 ;;
         sdcardfree)
                 # free Diskspace in Byte
-                df -P -B1| grep /dev/root | tr -s " " " " | cut -d " " -f 4
+                df -P -B1| grep /dev/root | tr -s " " | cut -d " " -f 4
                 ;;
         sdcardtotalsize)
                 # Size of SD-Card in Byte
-                df -P -B1 | grep /dev/root | tr -s " " " " | cut -d " " -f 2
+                df -P -B1 | grep /dev/root | tr -s " " | cut -d " " -f 2
                 ;;
         sdcardusedpercent)
                 # Used Diskspace in Percent
-                df -P -B1| grep /dev/root | tr -s " " " " | cut -d " " -f 5 | tr -d "%"
+                df -P -B1| grep /dev/root | tr -s " " | cut -d " " -f 5 | tr -d "%"
                 ;;
         sdcardused)
                 # Used Diskspace in Byte
-                df -P -B1| grep /dev/root | tr -s " " " " | cut -d " " -f 3
+                df -P -B1| grep /dev/root | tr -s " " | cut -d " " -f 3
                 ;;
         sdramcvoltage)
                 # sdram_c Voltage in Volt
-                /opt/vc/bin/vcgencmd measure_volts | tr -d "volt=" | tr -d "V"
+                /opt/vc/bin/vcgencmd measure_volts | tr -d "volt=V"
                 ;;
         sdramivoltage)
                 # sdram_i Voltage in Volt
-                /opt/vc/bin/vcgencmd measure_volts | tr -d "volt=" | tr -d "V"
+                /opt/vc/bin/vcgencmd measure_volts | tr -d "volt=V"
                 ;;
         sdrampvoltage)
                 # sdram_p Voltage in Volt
-                /opt/vc/bin/vcgencmd measure_volts | tr -d "volt=" | tr -d "V"
+                /opt/vc/bin/vcgencmd measure_volts | tr -d "volt=V"
                 ;;
         temperature)
-                # Temperature in 1/1000 centigrade
-                cat /sys/class/thermal/thermal_zone*/temp
+                # Temperature in degree celsius
+				/opt/vc/bin/vcgencmd measure_temp | tr -d "temp='C"
                 ;;
         *)
                 echo "Usage: $N {boardrevision|boardversion|boardserialnumber|coreclock|cpuvoltage|cpuclock|cpumem|firmwareversion|gpumem|sdcardtotalsize|sdcardused|sdcardusedpercent|sdcardfree|sdramcvoltage|sdramivoltage|sdrampvoltage|temperature}" >&2
